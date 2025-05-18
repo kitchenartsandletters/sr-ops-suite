1e7d6bad99d504c746800e7907fee0f65b8ba001
ghoulcauldron <gilcalderon07@gmail.com>
2025-05-18 01:27:00 -0400

    making sure app home also sorts by title when called

--- src/slack/app.js
+++ src/slack/app.js
@@ -166,6 +166,16 @@ module.exports = function registerSlackCommands(slackApp) {
       }
     }
     console.log('Using paginated /sr-backorders, page:', page, 'sortKey:', sortKey);
+    // If invoked with a sortKey, push directly to App Home
+    if (sortKey) {
+      await client.chat.postEphemeral({
+        channel: body.channel_id,
+        user: body.user_id,
+        text: `Publishing backorders (sorted by ${sortKey}) to your App Home…`
+      });
+      await publishBackordersHomeView(body.user_id, client, page, sortKey);
+      return;
+    }
     try {
       const { blocks, total } = await buildBackordersBlocks(page, sortKey);
       if (!blocks) {
